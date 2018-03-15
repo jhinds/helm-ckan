d0dc4efc603333709f1018389d3b6d3476705934
Jonathan Hinds <jhinds2010@gmail.com>
2018-03-15 09:50:07 -0400

    Added rq deployment & vaules file

---
+++ docker/ckan/bin/rq-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+# URL for solr (required unless linked to a container called 'solr')
+: ${CKAN_SOLR_URL:=}
+# URL for redis (required unless linked to a container called 'redis')
+: ${CKAN_REDIS_URL:=}
+
+CONFIG="${CKAN_CONFIG}/${CUSTOMER_CONFIG}"
+
+abort () {
+  echo "$@" >&2
+  exit 1
+}
+
+if [ -z "$CKAN_SOLR_URL" ]; then
+    abort "ERROR: no CKAN_SOLR_URL has been set"
+fi
+
+if [ -z "$CKAN_REDIS_URL" ]; then
+    abort "ERROR: no CKAN_REDIS_URL has been set"
+fi
+
+echo "Starting RQ Worker"
+paster --plugin=ckan jobs -c $CONFIG worker
